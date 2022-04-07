7571d67f9a38827acd4adc802a579e5b6505dac7
Mahantesh kumar <97968290+mantu-9194@users.noreply.github.com>
2022-04-07 18:51:15 +0530

    Add files via upload

---
+++ day15.js
@@ -0,0 +1,39 @@
+// $("#elem").click(function(){
+//     $("h3").hide()
+// })
+// $("#elem").click(function(){
+//     $(this).hide()
+// })
+
+
+// // ajax  it is a fn which takes an object of diff keys
+// $("button").click(function(){
+//     $.ajax({
+//         url:'https://jsonplaceholder.typicode.com/todo',
+//         type : "GET",
+//         success:function(data){
+//             console.log(data);
+//         }
+//     })
+// })
+
+
+//  crud 
+//  get post put delete
+ 
+//  js is functional progm lang
+// class A{
+//  constructor(name){
+//      this.username=name
+
+//  }
+//  printName(){
+//      console.log("name is :",this.username)
+//  }
+// }
+// var a=new A("sachin");
+// a.printName();
+// console.log(a.username)
+// class B{
+
+// }
